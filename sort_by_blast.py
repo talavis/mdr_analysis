375bf59d5b72881e63cd27489fcd8cffa6fa3f2d
Linus Östberg <linus@talavis.eu>
2016-09-12 12:53:38 +0200

    Added blast file reading

--- sort_by_blast.py
+++ sort_by_blast.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 import sys
+
 import bioinfo
 
-
-
+def read_blast(filename) :
+    '''Read a tabular BLAST output file'''
+    with open(filename) as infile :
+        raw = infile.read()
+        data = tuple(line.split('\t') for line in raw.split('\n') if len(line) > 0)
+    return data
+        
 def main() :
     if len(sys.argv) != 3 :
         sys.stderr.write('Usage: {} <BLAST tabular output> <FASTA file>\n'.format(sys.argv[0]))
         sys.exit(1)
 
-    
-        
+    blast_data = read_blast(sys.argv[1])
+    seqs, headers = bioinfo.read_fasta(sys.argv[2])
+
 if __name__ == '__main__' :
     main()
