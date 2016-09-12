7ffd734ab22ed93ca3dbdf9a7571d67a639833c0
Linus Östberg <linus@talavis.eu>
2016-09-12 14:17:46 +0200

    Added tests to bioinfo.py, including travis support. Also fixed a couple of bugs that were discovered by the tests

--- bioinfo.py
+++ bioinfo.py
@@ -4,11 +4,16 @@ import sys
 
 def beautify_fasta(sequence, per_line = 60) :
     '''Limit the number of characters per line for a sequence'''
-    i = 0
+    i = per_line
     while i < len(sequence) :
         sequence = sequence[:i] + '\n' + sequence[i:]
         i += per_line + 1
     return sequence
+
+def test_beautify_fasta() :
+    seq = 'MCTTGQVIRCKAAILWKPGAPFSIEEVEVAPPKAKEVRIKVVATGLCGTEMKVLGSKHLD' + 'MCTTGQVIRCKAAILWKPGAPFSIEEVEVAPPKAKEVRIKVVATGLCGTEMKVLGSKHLD'
+    assert beautify_fasta(seq) == 'MCTTGQVIRCKAAILWKPGAPFSIEEVEVAPPKAKEVRIKVVATGLCGTEMKVLGSKHLD' + '\n' + 'MCTTGQVIRCKAAILWKPGAPFSIEEVEVAPPKAKEVRIKVVATGLCGTEMKVLGSKHLD'
+    assert beautify_fasta(seq[:15]) == 'MCTTGQVIRCKAAILWKPGAPFSIEEVEVAPPKAKEVRIKVVATGLCGTEMKVLGSKHLD'[:15]
 
 def get_species(header) :
     '''Get the species of a sequence'''
@@ -17,8 +22,19 @@ def get_species(header) :
         return header[header.index('[')+1:header.index(']')]
     # UniProt
     if header[:2] in ['sp', 'tr'] :
-        return header[header.index('OS=')+3:header.index('=', header.index('OS=')+3)-2]
+        return header[header.index('OS=')+3:header.index('=', header.index('OS=')+3)-2].strip()
 
+    sys.stderr.write('E: Unable to identify species({})'.format(header))
+    return 'Unknown'
+
+def test_get_species() :
+    ncbi_head = 'gi|8393848|ref|NP_058905.1| trans-2-enoyl-CoA reductase, mitochondrial precursor [Rattus norvegicus]'
+    uniprot_head = 'sp|Q9BV79|MECR_HUMAN Trans-2-enoyl-CoA reductase, mitochondrial OS=Homo sapiens GN=MECR PE=1 SV=2'
+    ensembl_head = 'MECR-001 peptide: ENSP00000263702 pep:KNOWN_protein_coding'
+    assert get_species(ncbi_head) == 'Rattus norvegicus'
+    assert get_species(uniprot_head) == 'Homo sapiens'
+    assert get_species(ensembl_head) == 'Unknown'
+    
 def read_fasta(filename) :
     '''Read a FASTA file and return the headers and sequences as lists'''
     with open(filename) as infile :
@@ -31,7 +47,39 @@ def read_fasta(filename) :
             elif len(line.strip()) > 0 :
                 seqs[-1] += line.strip()
 
-    return headers, seqs
+    return (headers, seqs)
+
+def test_read_fasta() :
+    import tempfile
+
+    indata = '''>gi|8393848|ref|NP_058905.1| trans-2-enoyl-CoA reductase, mitochondrial precursor [Rattus norvegicus]
+MLVSRRLTGARARAPLLASLLEAWCRQGRTTSSYSAFSEPSHVRALVYGNHGDPAKVIQLKNLELTAVEG
+    SDVHVKMLAAPINPSDINMIQGNYGLLPKLPAVGGNEGVGQVIAVGSSVSGLKPGDWVIPANAGLGTWRT
+EAVFSEEALIGVPKDIPLQSAATLGVNPCTAYRMLVDFEQLQPGDSVIQNASNSGVGQAVIQIASALGLK
+    TINVIRDRPDIKKLTDRLKDLGADYVLTEEELRMPETKNIFKDLPLPRLALNCVGGKSSTELLRHLAPGG
+TMVTYGGMAKQPVTASVSMLIFKDLKLRGFWLSQWKKNHSPDEF
+KELILILCNLIRQGQLTAPAWSGIPL
+QDYQQALEASMKPFVSLKQILTM
+
+>gi|18408069|ref|NP_566881.1| putative trans-2-enoyl-CoA reductase [Arabidopsis thaliana]
+    MAALMESVVGRALKFSSTANFRSIRRGETPTLCIKSFSTIMSPPSKAIVYEEHGSPDSVTRLVNLPPVEV
+KENDVCVKMIAAPINPSDINRIEGVYPVRPPVPAVGGYEGVGEVYAVGSNVNGFSPGDWVIPSPPSSGTW
+
+
+QTYVVKEESVWHKIDKECPMEYAATITVNPLTALRMLEDFVNLNSGDSVVQNGATSIVGQCVIQLARLRG
+ISTINLIRDRAGSDEAREQLKALGADEVFSESQLNVKNVKSLLGNLPEPALGFNCVGGNAASLVLKYLRE
+GGTMVTYGGMSKKPITVSTTSFIFKDLALRGFWLQSWLSMGKVKECREMIDYLLGLARDGKLKYETELVP
+FEEFPVALDKALGKLGRQPKQVITF'''
+
+    realhead = ['gi|8393848|ref|NP_058905.1| trans-2-enoyl-CoA reductase, mitochondrial precursor [Rattus norvegicus]', 'gi|18408069|ref|NP_566881.1| putative trans-2-enoyl-CoA reductase [Arabidopsis thaliana]']
+    realseq = ['MLVSRRLTGARARAPLLASLLEAWCRQGRTTSSYSAFSEPSHVRALVYGNHGDPAKVIQLKNLELTAVEGSDVHVKMLAAPINPSDINMIQGNYGLLPKLPAVGGNEGVGQVIAVGSSVSGLKPGDWVIPANAGLGTWRTEAVFSEEALIGVPKDIPLQSAATLGVNPCTAYRMLVDFEQLQPGDSVIQNASNSGVGQAVIQIASALGLKTINVIRDRPDIKKLTDRLKDLGADYVLTEEELRMPETKNIFKDLPLPRLALNCVGGKSSTELLRHLAPGGTMVTYGGMAKQPVTASVSMLIFKDLKLRGFWLSQWKKNHSPDEFKELILILCNLIRQGQLTAPAWSGIPLQDYQQALEASMKPFVSLKQILTM',
+               'MAALMESVVGRALKFSSTANFRSIRRGETPTLCIKSFSTIMSPPSKAIVYEEHGSPDSVTRLVNLPPVEVKENDVCVKMIAAPINPSDINRIEGVYPVRPPVPAVGGYEGVGEVYAVGSNVNGFSPGDWVIPSPPSSGTWQTYVVKEESVWHKIDKECPMEYAATITVNPLTALRMLEDFVNLNSGDSVVQNGATSIVGQCVIQLARLRGISTINLIRDRAGSDEAREQLKALGADEVFSESQLNVKNVKSLLGNLPEPALGFNCVGGNAASLVLKYLREGGTMVTYGGMSKKPITVSTTSFIFKDLALRGFWLQSWLSMGKVKECREMIDYLLGLARDGKLKYETELVPFEEFPVALDKALGKLGRQPKQVITF']
+
+    filename = tempfile.mkstemp()[1]
+    with open(filename, 'w') as f :
+        f.write(indata)
+
+    assert read_fasta(filename) == (realhead, realseq)
     
 if __name__ == '__main__' :
     sys.exit()
