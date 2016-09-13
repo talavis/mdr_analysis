dcd30440e9078859d30fc37fcd3a4908bbc189ef
Linus Östberg <linus@talavis.eu>
2016-09-13 15:23:59 +0200

    Added a step with MAFFT alignment after filtering. Number of threads determined by nproc.

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@
 SCRIPTDIR=.
 ${SCRIPTDIR}/sort_by_blast.py $1 $2 > ${1}_sorted
 ${SCRIPTDIR}/filter.py ${1}_sorted $3 > ${1}_filtered
+mafft-linsi --thread `nproc` ${1}_filtered > ${1}_filtered.mali
