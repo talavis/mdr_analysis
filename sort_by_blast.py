cd3a4dd872e5185fe10823eab527c7da70902c9c
Linus Östberg <linus@talavis.eu>
2016-09-15 13:34:10 +0200

    Added test for main()

--- sort_by_blast.py
+++ sort_by_blast.py
@@ -15,8 +15,7 @@ def test_read_blast() :
     import tempfile
 
     indata = '''sp|Q9BV79|MECR_HUMAN	gi|544346134|ref|NP_057095.3|	99.732	373	1	0	1	373	1	373	0.0	763
-sp|Q9BV79|MECR_HUMAN	gi|397515849|ref|XP_003828155.1|	99.196	373	3	0	1	373	1	373	0.0	757
-'''
+sp|Q9BV79|MECR_HUMAN	gi|397515849|ref|XP_003828155.1|	99.196	373	3	0	1	373	1	373	0.0	757'''
     filename = tempfile.mkstemp()[1]
     with open(filename, 'w') as f :
         f.write(indata)
@@ -56,6 +55,38 @@ def main(blast_file, filename) :
         if ind != False :
             print('>{}\n{}'.format(headers[ind], bioinfo.beautify_fasta(seqs[ind])))
 
+def test_main(capsys) :
+    import tempfile
+    
+    indata_blast = '''sp|Q9BV79|MECR_HUMAN	gi|0000001|ref|NP_000001.1|	99.732	373	1	0	1	373	1	373	0.0	763
+sp|Q9BV79|MECR_HUMAN	gi|0000003|ref|NP_000003.1|	99.196	373	3	0	1	373	1	373	0.0	757'''
+    filename_blast = tempfile.mkstemp()[1]
+    with open(filename_blast, 'w') as f :
+        f.write(indata_blast)
+
+    indata_fasta = '''>gi|0000000|ref|NP_000000.1| Made-up data [Rattus norvegicus]
+ACDEFGHIKL
+>gi|0000001|ref|NP_000001.1| Made-up data [Arabidopsis thaliana]
+ACDEAGHIKL
+>gi|0000002|ref|NP_000002.1| Made-up data [Homo Sapiens]
+ACDEFGHIKL
+>gi|0000003|ref|NP_000003.1| Made-up data [Arabidopsis thaliana]
+ADDEEGHILL'''
+    
+    filename_fasta = tempfile.mkstemp()[1]
+    with open(filename_fasta, 'w') as f :
+        f.write(indata_fasta)
+
+    REAL = '''>gi|0000001|ref|NP_000001.1| Made-up data [Arabidopsis thaliana]
+ACDEAGHIKL
+>gi|0000003|ref|NP_000003.1| Made-up data [Arabidopsis thaliana]
+ADDEEGHILL
+'''
+    main(filename_blast, filename_fasta)
+    out, err = capsys.readouterr()
+    assert out == REAL
+
+    
 if __name__ == '__main__' :
     if len(sys.argv) != 3 :
         sys.stderr.write('Usage: {} <BLAST tabular output> <FASTA file>\n'.format(sys.argv[0]))
