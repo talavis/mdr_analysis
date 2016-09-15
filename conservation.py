4ff3d39685bab02fec24baff2d2bc953f8423924
Linus Östberg <linus@talavis.eu>
2016-09-15 11:01:43 +0200

    Got the main() function working again after renaming of functions

--- conservation.py
+++ conservation.py
@@ -28,7 +28,13 @@ def get_most_conserved(freq_table, align_len) :
     '''Determine the most conserved residue and its conservation rate in each position
     Input should be a PSSM object or a list of dicts (PSSM.pssm, but without [0] in each tuple)'''
     result = list()
-    for p in range(len(freq_table)) :
+    # depending on input type
+    try :
+        num_positions = len(freq_table)
+    except TypeError :
+        num_positions = len(freq_table.pssm)
+    
+    for p in range(num_positions) :
         freq_table_inv = dict((j, i) for i, j in freq_table[p].items())
         best_conserved = freq_table_inv[max(freq_table_inv)]
         score = freq_table[p][best_conserved]/align_len
@@ -77,25 +83,23 @@ def test_make_freq_table() :
     
 def main() :
     if len(sys.argv) != 3 :
-        sys.stderr.write('Usage: {0} <Alignment> <reference sequence>\n'.format(sys.argv[0]))
+        sys.stderr.write('Usage: {0} <alignment> <reference sequence>\n'.format(sys.argv[0]))
         sys.exit(1)
 
     REFSEQ = sys.argv[2]
 
     # load alignments
     filename = sys.argv[1]
-
-    alignment = read_fasta_bp(filename)
+    alignment = AlignIO.read(filename, 'fasta' )
 
     headers = [s.name for s in alignment]
     REFIND = headers.index([h for h in headers if REFSEQ in h][0])
     SEQLEN = len(str(alignment[REFIND].seq).replace('-', ''))
 
     freq_table = make_freq_table(alignment)
-    cons = calculate_conservation(freq_table, len(alignment))
+    cons = get_most_conserved(freq_table, len(alignment))
 
     for i in range(len(freq_table.pssm)) :
-        print(freq_table[i])
         print(cons[i])
 
 if __name__ == '__main__' :
