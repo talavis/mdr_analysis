d93fd2ed16d738287733b9cfc2128731c9c4ec5f
Linus Östberg <linus@talavis.eu>
2016-09-30 16:03:13 +0200

    Another attempt to get travis working

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-py.test -vv tests/test_bioinfo.py --cov=bioinfo\
-	    tests/test_conservation.py --cov=conservation\
-	    tests/test_sequence_filter.py --cov=sequence_filter\
-	    tests/test_sort_by_blast.py --cov=sort_by_blast\
-	    tests/test_species.py --cov=species\
-	    tests/test_taxnode.py --cov=taxnode\
+PYTHONPATH=. py.test -vv tests/test_bioinfo.py --cov=bioinfo\
+	                 tests/test_conservation.py --cov=conservation\
+	                 tests/test_sequence_filter.py --cov=sequence_filter\
+	                 tests/test_sort_by_blast.py --cov=sort_by_blast\
+	                 tests/test_species.py --cov=species\
+	                 tests/test_taxnode.py --cov=taxnode\
