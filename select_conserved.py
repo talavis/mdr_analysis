d0205ced9147177d701516d2336593a44072e38d
Linus Östberg <linus@talavis.eu>
2016-11-14 14:02:16 +0100

    incorrect parameter number

--- select_conserved.py
+++ select_conserved.py
@@ -37,4 +37,4 @@ if __name__ == '__main__':
     if len(sys.argv) == 2:
         main(sys.argv[1])
     else:
-        main(sys.argv[1], float(sys.argv[3]))
+        main(sys.argv[1], float(sys.argv[2]))
