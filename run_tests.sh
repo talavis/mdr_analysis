08cdda6e20bee58fe4e2057810118f487070bf09
Linus Östberg <linus@talavis.eu>
2016-12-01 13:14:43 +0100

    map_icm_cons_as is now tested.
    Changed the order of the tests to match filename order.
    
    Also did some whitespace changes.

--- run_tests.sh
+++ run_tests.sh
@@ -3,14 +3,15 @@
 TESTDIR=tests
 
 PYTHONPATH=.:find_sequences py.test -vvv ${TESTDIR}/test_bioinfo.py --cov=bioinfo\
-	                                 ${TESTDIR}/test_conservation.py --cov=conservation\
-	                                 ${TESTDIR}/test_gen_icm_visual.py --cov=gen_icm_visual\
-					 ${TESTDIR}/test_map_cons_to_ali.py --cov=map_cons_to_ali\
+					 ${TESTDIR}/test_conservation.py --cov=conservation\
+					 ${TESTDIR}/test_gen_icm_visual.py --cov=gen_icm_visual\
 					 ${TESTDIR}/test_map_as_to_ali.py --cov=map_as_to_ali\
+					 ${TESTDIR}/test_map_cons_to_ali.py --cov=map_cons_to_ali\
+ 					 ${TESTDIR}/test_map_icm_cons_as.py --cov=map_icm_cons_as\
 					 ${TESTDIR}/test_select_conserved.py --cov=select_conserved\
 					 ${TESTDIR}/test_sequence_filter.py --cov=sequence_filter\
 					 ${TESTDIR}/test_seq_id.py --cov=seq_id\
-	                                 ${TESTDIR}/test_sort_by_blast.py --cov=sort_by_blast\
-	                                 ${TESTDIR}/test_species.py --cov=species\
-	                                 ${TESTDIR}/test_taxnode.py --cov=taxnode\
-                                         ${TESTDIR}/find_sequences/test_select_accs_blastp.py --cov=select_accs_blastp\
+					 ${TESTDIR}/test_sort_by_blast.py --cov=sort_by_blast\
+					 ${TESTDIR}/test_species.py --cov=species\
+					 ${TESTDIR}/test_taxnode.py --cov=taxnode\
+					 ${TESTDIR}/find_sequences/test_select_accs_blastp.py --cov=select_accs_blastp
