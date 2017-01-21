9f88537366845091709f775c33aa71b8357d890b
Linus Östberg <linus@talavis.eu>
2017-01-21 09:45:45 +0100

    forgot to add select_seq_by_species to the tests that are run

--- run_tests.sh
+++ run_tests.sh
@@ -11,6 +11,7 @@ PYTHONPATH=.:find_sequences py.test -vvv ${TESTDIR}/test_bioinfo.py --cov=bioinf
  					 ${TESTDIR}/test_map_icm_cons_as.py --cov=map_icm_cons_as\
 					 ${TESTDIR}/test_select_conserved.py --cov=select_conserved\
 					 ${TESTDIR}/test_sequence_filter.py --cov=sequence_filter\
+					 ${TESTDIR}/test_seq_by_species.py --cov=seq_by_species\
 					 ${TESTDIR}/test_seq_id.py --cov=seq_id\
 					 ${TESTDIR}/test_sort_by_blast.py --cov=sort_by_blast\
 					 ${TESTDIR}/test_species.py --cov=species\
