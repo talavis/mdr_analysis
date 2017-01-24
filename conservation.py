7ec2f63a358902b990d5af62875bd1fb0f0db628
Linus Östberg <linus@talavis.eu>
2017-01-24 15:52:28 +0100

    pylint/flake8 fixes

--- conservation.py
+++ conservation.py
@@ -8,8 +8,12 @@ from Bio.Align import AlignInfo
 
 
 def get_most_conserved(freq_table, align_len):
-    '''Determine the most conserved residue and its conservation rate in each position
-    Input should be a PSSM object or a list of dicts (PSSM.pssm, but without [0] in each tuple)'''
+    '''
+    Determine the most conserved residue and its conservation
+    rate in each position.
+    Input should be a PSSM object or a list of dicts
+    (PSSM.pssm, but without [0] in each tuple)
+    '''
     result = list()
     # depending on input type
     try:
@@ -18,13 +22,14 @@ def get_most_conserved(freq_table, align_len):
         num_positions = len(freq_table.pssm)
 
     for pos in range(num_positions):
-        if list(freq_table[pos].values()).count(max(freq_table[pos].values())) == 1: 
+        values = list(freq_table[pos].values())
+        if values.count(max(values)) == 1:
             freq_table_inv = dict((j, i) for i, j in freq_table[pos].items())
             best_conserved = freq_table_inv[max(freq_table_inv)]
             score = freq_table[pos][best_conserved]/align_len
         else:
             best_conserved = 'X'
-            score = max(freq_table[pos].values())/align_len
+            score = max(values)/align_len
         result.append((score, best_conserved))
 
     return result
@@ -50,7 +55,8 @@ def main(filename, refseq=None):
         try:
             refind = headers.index([h for h in headers if refseq in h][0])
         except IndexError:
-            error = 'E: The reference sequence ({}) not found among the sequences\n'.format(refseq)
+            error = ('E: The reference sequence ({}) ' +
+                     'not found among the sequences\n').format(refseq)
             sys.stderr.write(error)
             return False
 
@@ -62,18 +68,20 @@ def main(filename, refseq=None):
     else:
         refseq_p = str(alignment[refind].seq)
         print('# {}'.format(refseq))
-    p = 1
+    pos = 1
     for i in range(len(freq_table.pssm)):
-#        if cons[i][1] != '-':
         if refseq_p[i] != '-':
-            print('{ps}\t{rs}\t{mc}\t{rate:.3}'.format(ps=p, rs=refseq_p[i],
-                                                           mc=cons[i][1], rate=cons[i][0]))
-            p += 1
+            print('{ps}\t{rs}\t{mc}\t{rate:.3}'.format(ps=pos,
+                                                       rs=refseq_p[i],
+                                                       mc=cons[i][1],
+                                                       rate=cons[i][0]))
+            pos += 1
 
 
 if __name__ == '__main__':
     if len(sys.argv) not in (2, 3):
-        sys.stderr.write('Usage: {0} <alignment file> [reference seq]\n'.format(sys.argv[0]))
+        sys.stderr.write(('Usage: {0} '.format(sys.argv[0]) +
+                          '<alignment file> [reference seq]\n'))
         sys.exit(1)
 
     if len(sys.argv) == 3:
