7a1f4827485a4105d92c2779aca9e4e200f493db
Linus Östberg <linus@talavis.eu>
2017-07-06 22:18:32 +0200

    Added code to generate a tree for each position in an alignment

---
+++ gen_postrees.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''
+Generate a set of treefiles with sequence names corresponding
+to the columns of a FASTA alignment
+'''
+
+import sys
+
+import bioinfo
+
+def main(ali_filename, tree_filename):
+    '''
+    Generate a set of treefiles with sequence names corresponding
+    to the columns of a FASTA alignment
+    '''
+    ali = bioinfo.read_fasta(ali_filename)
+    tree_raw = open(tree_filename).read()
+
+    basename = ali_filename[:ali_filename.index('.')]
+    for i in range(len(ali[1][1])):
+        tree_new = tree_raw
+        tree_out = open(basename + str(i+1) + '.tree', 'w')
+        # assume uniprot headers; accession code between 1st and 2nd |
+        # in other words: str[3:str.index('|', 3)
+        for head in range(len(ali[0])):
+            acc = ali[0][head][3:ali[0][head].index('|', 3)]
+            tree_new = tree_new.replace(acc, '{}_{}'.format(ali[1][head][i], acc))
+        tree_out.write(tree_new)
+        tree_out.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        sys.stderr.write('Usage: {} <FASTA alignment> <tree file>\n'.format(sys.argv[0]))
+        sys.exit(1)
+    main(sys.argv[1], sys.argv[2])
