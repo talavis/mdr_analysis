533f62a565269b434c6c0f17380ce5a2769e2d43
Linus Östberg <linus@talavis.eu>
2017-09-08 15:38:55 +0200

    conservation function working

--- analyse_ali_groups.py
+++ analyse_ali_groups.py
@@ -8,19 +8,36 @@ import sys
 
 import bioinfo
 
-def analyse_ali(sequences, groups = True):
+
+GROUPS = {'I':'B'}
+
+
+def conservation(sequences):
     '''
     Analyse an alignment based on conservation.
-    Apart from only analysing exact residues,
-    also allow analysis based on property groups,
-    e.g. hydrophobic.
     '''
+    best_cons = [0] * len(sequences[0])
+    nseqs = len(sequences)
+    for i in range(len(sequences[0])):
+        pos_res = tuple(seq[i] for seq in sequences)
+        res_counts = tuple((res, pos_res.count(res)/nseqs) for res in set(pos_res))
+        counts = tuple(count[1] for count in res_counts)
+        best_cons[i] = res_counts[counts.index(max(counts))]
+    return best_cons
 
 
-def test_analyse_ali():
+def test_conservation():
     '''
     Test analyse_ali()
     '''
+    seqs = ('ACDEF',
+            'ACEEF',
+            'ACDEE',
+            'CCDDD',
+            'DCADF')
+    expected = list(zip('ACDEF', (0.6, 1.0, 0.6, 0.6, 0.6)))
+    assert conservation(seqs) == expected
+
 
 
 def main(filename):
@@ -38,6 +55,6 @@ def test_main(capsys):
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        sys.stderr.write('Usage: {} <FASTA alignment>\n'format(sys.argv[0]))
+        sys.stderr.write('Usage: {} <FASTA alignment>\n'.format(sys.argv[0]))
         sys.exit(1)
     main(sys.argv[1])
